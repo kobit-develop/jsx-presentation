147b901f7001b6dd5d7c1ca5c01b24945a0d5ea0
mottox2 <y.mottox2@gmail.com>
2019-08-07 16:10:29 +0900

    Generate pptx

---
+++ index.js
@@ -0,0 +1,166 @@
+const fs = require('fs')
+const JSZip = require('jszip')
+
+const zip = new JSZip()
+
+const genAppXml = (slides = [], companyName = 'My Corp') => `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/extended-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">
+  <Application>Microsoft Office PowerPoint</Application>
+  <Slides>${slides.length}</Slides>
+  <ScaleCrop>false</ScaleCrop>
+  <HeadingPairs>
+    <vt:vector size="4" baseType="variant">
+      <vt:variant>
+        <vt:lpstr>Theme</vt:lpstr>
+      </vt:variant>
+      <vt:variant>
+        <vt:i4>1</vt:i4>
+      </vt:variant>
+      <vt:variant>
+        <vt:lpstr>Slide Titles</vt:lpstr>
+      </vt:variant>
+      <vt:variant>
+        <vt:i4>1</vt:i4>
+      </vt:variant>
+    </vt:vector>
+  </HeadingPairs>
+  <TitlesOfParts>
+    <vt:vector size="1" baseType="lpstr">
+      <vt:lpstr>Office Theme</vt:lpstr>
+    </vt:vector>
+  </TitlesOfParts>
+  <Company>${companyName}</Company>
+  <LinksUpToDate>false</LinksUpToDate>
+  <SharedDoc>false</SharedDoc>
+  <HyperlinksChanged>false</HyperlinksChanged>
+  <AppVersion>12.0000</AppVersion>
+</Properties>
+`
+
+const genCoreXml = () => `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<cp:coreProperties xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:dcmitype="http://purl.org/dc/dcmitype/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <dc:creator>mottox2</dc:creator>
+  <cp:lastModifiedBy>mottox2</cp:lastModifiedBy>
+  <dcterms:created xsi:type="dcterms:W3CDTF">2019-08-07T01:43:14Z</dcterms:created>
+  <dcterms:modified xsi:type="dcterms:W3CDTF">2019-08-07T01:43:14Z</dcterms:modified>
+  <dc:title>Sample Slide</dc:title>
+  <dc:description>Sample Slide</dc:description>
+  <dc:subject>mottox2</dc:subject>
+  <cp:keywords>office 2007 openxml libreoffice odt php</cp:keywords>
+  <cp:category>category Category</cp:category>
+</cp:coreProperties>
+`
+
+const theme1 = fs.readFileSync('./theme1.xml')
+const presentation = fs.readFileSync('./presentation.xml')
+const slideLayout1 = fs.readFileSync('./slideLayout1.xml')
+const slideMaster1 = fs.readFileSync('./slideMaster1.xml')
+const slide1 = fs.readFileSync('./slideMaster1.xml')
+
+zip.folder("_rels");
+zip.folder("docProps");
+zip.folder("ppt").folder("_rels");
+// zip.folder("ppt/charts").folder("_rels");
+// zip.folder("ppt/embeddings");
+// zip.folder("ppt/media");
+zip.folder("ppt/slideLayouts").folder("_rels");
+zip.folder("ppt/slideMasters").folder("_rels");
+zip.folder("ppt/slides").folder("_rels");
+zip.folder("ppt/theme");
+
+zip.file("[Content_Types].xml", `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
+  <Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/>
+  <Default Extension="xml" ContentType="application/xml"/>
+  <Override PartName="/ppt/presentation.xml" ContentType="application/vnd.openxmlformats-officedocument.presentationml.presentation.main+xml"/>
+  <Override PartName="/ppt/presProps.xml" ContentType="application/vnd.openxmlformats-officedocument.presentationml.presProps+xml"/>
+  <Override PartName="/ppt/tableStyles.xml" ContentType="application/vnd.openxmlformats-officedocument.presentationml.tableStyles+xml"/>
+  <Override PartName="/ppt/viewProps.xml" ContentType="application/vnd.openxmlformats-officedocument.presentationml.viewProps+xml"/>
+  <Override PartName="/docProps/app.xml" ContentType="application/vnd.openxmlformats-officedocument.extended-properties+xml"/>
+  <Override PartName="/docProps/core.xml" ContentType="application/vnd.openxmlformats-package.core-properties+xml"/>
+  <Override PartName="/docProps/custom.xml" ContentType="application/vnd.openxmlformats-officedocument.custom-properties+xml"/>
+  <Override PartName="/ppt/slideMasters/slideMaster1.xml" ContentType="application/vnd.openxmlformats-officedocument.presentationml.slideMaster+xml"/>
+  <Override PartName="/ppt/theme/theme1.xml" ContentType="application/vnd.openxmlformats-officedocument.theme+xml"/>
+  <Override PartName="/ppt/slideLayouts/slideLayout1.xml" ContentType="application/vnd.openxmlformats-officedocument.presentationml.slideLayout+xml"/>
+  <Override PartName="/ppt/slides/slide1.xml" ContentType="application/vnd.openxmlformats-officedocument.presentationml.slide+xml"/>
+  <Default Extension="gif" ContentType="image/gif"/>
+  <Default Extension="jpg" ContentType="image/jpeg"/>
+  <Default Extension="jpeg" ContentType="image/jpeg"/>
+  <Default Extension="png" ContentType="image/png"/>
+  <Default Extension="xlsx" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"/>
+</Types>
+`);
+// zip.file("_rels/.rels", '');
+zip.file("docProps/app.xml", genAppXml([null]));
+zip.file("docProps/core.xml", genCoreXml());
+zip.file("docProps/custom.xml", `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/custom-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes"/>
+`);
+
+zip.file("ppt/_rels/presentation.xml.rels", `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+  <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/slideMaster" Target="slideMasters/slideMaster1.xml"/>
+  <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/theme" Target="theme/theme1.xml"/>
+  <Relationship Id="rId3" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/slide" Target="slides/slide1.xml"/>
+  <Relationship Id="rId4" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/presProps" Target="presProps.xml"/>
+  <Relationship Id="rId5" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/viewProps" Target="viewProps.xml"/>
+  <Relationship Id="rId6" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/tableStyles" Target="tableStyles.xml"/>
+</Relationships>
+`)
+
+zip.file("ppt/theme/theme1.xml", theme1);
+zip.file("ppt/presentation.xml", presentation);
+zip.file("ppt/presProps.xml", `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<p:presentationPr xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:p="http://schemas.openxmlformats.org/presentationml/2006/main">
+  <p:extLst>
+    <p:ext uri="{E76CE94A-603C-4142-B9EB-6D1370010A27}">
+      <p14:discardImageEditData xmlns:p14="http://schemas.microsoft.com/office/powerpoint/2010/main" val="0"/>
+    </p:ext>
+    <p:ext uri="{D31A062A-798A-4329-ABDD-BBA856620510}">
+      <p14:defaultImageDpi xmlns:p14="http://schemas.microsoft.com/office/powerpoint/2010/main" val="220"/>
+    </p:ext>
+  </p:extLst>
+</p:presentationPr>
+`);
+zip.file("ppt/tableStyles.xml", `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<a:tblStyleLst xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" def="{5C22544A-7EE6-4342-B048-85BDC9FD1C3A}"/>
+`);
+zip.file("ppt/viewProps.xml", `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<p:viewPr xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:p="http://schemas.openxmlformats.org/presentationml/2006/main" showComments="0" lastView="sldView">
+  <p:slideViewPr>
+    <p:cSldViewPr>
+      <p:cViewPr>
+        <p:scale>
+          <a:sx d="100" n="100"/>
+          <a:sy d="100" n="100"/>
+        </p:scale>
+        <p:origin x="0" y="0"/>
+      </p:cViewPr>
+    </p:cSldViewPr>
+  </p:slideViewPr>
+</p:viewPr>
+`);
+
+zip.file("ppt/slideLayouts/slideLayout1.xml", slideLayout1)
+zip.file("ppt/slideLayouts/_rels/slideLayout1.xml.rels", `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+  <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/slideMaster" Target="../slideMasters/slideMaster1.xml"/>
+</Relationships>
+`)
+
+zip.file("ppt/slideMasters/slideMaster1.xml", slideMaster1)
+zip.file("ppt/slideMasters/_rels/slideMaster1.xml.rels", `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+  <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/slideLayout" Target="../slideLayouts/slideLayout1.xml"/>
+  <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/theme" Target="../theme/theme1.xml"/>
+</Relationships>
+`)
+
+zip.file("ppt/slides/slide1.xml", slide1)
+zip.file("ppt/slides/_rels/slide1.xml.rels", `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
+  <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/slideLayout" Target="../slideLayouts/slideLayout1.xml"/>
+</Relationships>
+`)
+
+zip.generateAsync({type:'nodebuffer'}).then(function(content){ fs.writeFile('results/result.pptx', content, function(){} ); });
