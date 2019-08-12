99dd4242e0b8f1c484fd7d0f5b5f30db9b6a03c9
mottox2 <y.mottox2@gmail.com>
2019-08-12 21:41:50 +0900

    Create presentation.xml programmatically

--- src/generate.js
+++ src/generate.js
@@ -59,7 +59,6 @@ const generate = (tree) => {
   `
 
   const theme1 = fs.readFileSync('./xml/theme1.xml')
-  const presentation = fs.readFileSync('./xml/presentation.xml')
   const slideLayout1 = fs.readFileSync('./xml/slideLayout1.xml')
   const slideMaster1 = fs.readFileSync('./xml/slideMaster1.xml')
 
@@ -80,7 +79,6 @@ const generate = (tree) => {
   )
 
   zip.file('ppt/theme/theme1.xml', theme1)
-  zip.file('ppt/presentation.xml', presentation)
   zip.file(
     'ppt/presProps.xml',
     `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
@@ -219,6 +217,118 @@ const generate = (tree) => {
     <Relationship Id="rId${slides.length + 5}" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/tableStyles" Target="tableStyles.xml"/>
   </Relationships>
   `)
+
+  zip.file(
+    'ppt/presentation.xml',
+    `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+    <p:presentation xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:p="http://schemas.openxmlformats.org/presentationml/2006/main">
+      <p:sldMasterIdLst>
+        <p:sldMasterId id="2147483648" r:id="rId1"/>
+      </p:sldMasterIdLst>
+      <p:sldIdLst>
+        ${slides.map((slide, index) => {
+          return `<p:sldId id="${256 + index}" r:id="rId${3 + index}"/>`
+        }).join("\n")}
+      </p:sldIdLst>
+      <p:sldSz cx="9144000" cy="6858000" type="screen4x3"/>
+      <p:notesSz cx="6858000" cy="9144000"/>
+    <p:defaultTextStyle>
+      <a:defPPr>
+       <a:defRPr lang="fr-FR"/>
+      </a:defPPr>
+      <a:lvl1pPr algn="l" defTabSz="914400" eaLnBrk="1" hangingPunct="1" latinLnBrk="0" marL="0" rtl="0">
+       <a:defRPr kern="1200" sz="1800">
+        <a:solidFill>
+         <a:schemeClr val="tx1"/>
+        </a:solidFill>
+        <a:latin typeface="+mn-lt"/>
+        <a:ea typeface="+mn-ea"/>
+        <a:cs typeface="+mn-cs"/>
+       </a:defRPr>
+      </a:lvl1pPr>
+      <a:lvl2pPr algn="l" defTabSz="914400" eaLnBrk="1" hangingPunct="1" latinLnBrk="0" marL="457200" rtl="0">
+       <a:defRPr kern="1200" sz="1800">
+        <a:solidFill>
+         <a:schemeClr val="tx1"/>
+        </a:solidFill>
+        <a:latin typeface="+mn-lt"/>
+        <a:ea typeface="+mn-ea"/>
+        <a:cs typeface="+mn-cs"/>
+       </a:defRPr>
+      </a:lvl2pPr>
+      <a:lvl3pPr algn="l" defTabSz="914400" eaLnBrk="1" hangingPunct="1" latinLnBrk="0" marL="914400" rtl="0">
+       <a:defRPr kern="1200" sz="1800">
+        <a:solidFill>
+         <a:schemeClr val="tx1"/>
+        </a:solidFill>
+        <a:latin typeface="+mn-lt"/>
+        <a:ea typeface="+mn-ea"/>
+        <a:cs typeface="+mn-cs"/>
+       </a:defRPr>
+      </a:lvl3pPr>
+      <a:lvl4pPr algn="l" defTabSz="914400" eaLnBrk="1" hangingPunct="1" latinLnBrk="0" marL="1371600" rtl="0">
+       <a:defRPr kern="1200" sz="1800">
+        <a:solidFill>
+         <a:schemeClr val="tx1"/>
+        </a:solidFill>
+        <a:latin typeface="+mn-lt"/>
+        <a:ea typeface="+mn-ea"/>
+        <a:cs typeface="+mn-cs"/>
+       </a:defRPr>
+      </a:lvl4pPr>
+      <a:lvl5pPr algn="l" defTabSz="914400" eaLnBrk="1" hangingPunct="1" latinLnBrk="0" marL="1828800" rtl="0">
+       <a:defRPr kern="1200" sz="1800">
+        <a:solidFill>
+         <a:schemeClr val="tx1"/>
+        </a:solidFill>
+        <a:latin typeface="+mn-lt"/>
+        <a:ea typeface="+mn-ea"/>
+        <a:cs typeface="+mn-cs"/>
+       </a:defRPr>
+      </a:lvl5pPr>
+      <a:lvl6pPr algn="l" defTabSz="914400" eaLnBrk="1" hangingPunct="1" latinLnBrk="0" marL="2286000" rtl="0">
+       <a:defRPr kern="1200" sz="1800">
+        <a:solidFill>
+         <a:schemeClr val="tx1"/>
+        </a:solidFill>
+        <a:latin typeface="+mn-lt"/>
+        <a:ea typeface="+mn-ea"/>
+        <a:cs typeface="+mn-cs"/>
+       </a:defRPr>
+      </a:lvl6pPr>
+      <a:lvl7pPr algn="l" defTabSz="914400" eaLnBrk="1" hangingPunct="1" latinLnBrk="0" marL="2743200" rtl="0">
+       <a:defRPr kern="1200" sz="1800">
+        <a:solidFill>
+         <a:schemeClr val="tx1"/>
+        </a:solidFill>
+        <a:latin typeface="+mn-lt"/>
+        <a:ea typeface="+mn-ea"/>
+        <a:cs typeface="+mn-cs"/>
+       </a:defRPr>
+      </a:lvl7pPr>
+      <a:lvl8pPr algn="l" defTabSz="914400" eaLnBrk="1" hangingPunct="1" latinLnBrk="0" marL="3200400" rtl="0">
+       <a:defRPr kern="1200" sz="1800">
+        <a:solidFill>
+         <a:schemeClr val="tx1"/>
+        </a:solidFill>
+        <a:latin typeface="+mn-lt"/>
+        <a:ea typeface="+mn-ea"/>
+        <a:cs typeface="+mn-cs"/>
+       </a:defRPr>
+      </a:lvl8pPr>
+      <a:lvl9pPr algn="l" defTabSz="914400" eaLnBrk="1" hangingPunct="1" latinLnBrk="0" marL="3657600" rtl="0">
+       <a:defRPr kern="1200" sz="1800">
+        <a:solidFill>
+         <a:schemeClr val="tx1"/>
+        </a:solidFill>
+        <a:latin typeface="+mn-lt"/>
+        <a:ea typeface="+mn-ea"/>
+        <a:cs typeface="+mn-cs"/>
+       </a:defRPr>
+      </a:lvl9pPr>
+     </p:defaultTextStyle>
+    </p:presentation>`
+  )
 
   zip.file('docProps/app.xml', genAppXml(slides))
 
