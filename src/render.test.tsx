a7de3b4f25abdeed5ba85d80e3202b3a91b03559
mottox2 <y.mottox2@gmail.com>
2019-10-09 17:16:05 +0900

    Tweak calculating layouts

--- src/render.test.tsx
+++ src/render.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import render, { renderSlide, walkTree, composeJSONTree } from './render'
+import render, { renderSlide, composeNodeTree, composeLayoutedTree } from './render'
 import testRenderer from 'react-test-renderer'
 import Yoga from 'yoga-layout'
 
@@ -12,39 +12,43 @@ const logNode = (node: Yoga.Node, level = 0) => {
 }
 
 describe('render', () => {
-  // it('has default relationship', () => {
-  //   const json = testRenderer.create(<slide><text>aa</text></slide>).toJSON()
-  //   const { relationships } = renderSlide(json!, {
-  //     slides: [],
-  //     charts: []
-  //   })
-  //   console.log(relationships)
-  //   expect(relationships).toMatchObject([{
-  //     rId: 1, type: 'slideLayout', id: 1
-  //   }])
-  // })
-
-  // it('generate slide json', () => {
-  //   const { slides } = render(<presentation>
-  //     <slide><text>Hello World</text></slide>
-  //   </presentation>)
-  //   expect(slides.length).toBe(1)
-  //   expect(slides[0].relationships.length).toBe(1)
-  // })
-
+  it('has default relationship', () => {
+    const json = testRenderer.create(<slide><text>aa</text></slide>).toJSON()
+    const { relationships } = renderSlide(json!, {
+      slides: [],
+      charts: []
+    })
+    console.log(relationships)
+    expect(relationships).toMatchObject([{
+      rId: 1, type: 'slideLayout', id: 1
+    }])
+  })
 
   it('generate slide json', () => {
+    const { slides } = render(<presentation>
+      <slide><text>Hello World</text></slide>
+    </presentation>)
+    expect(slides.length).toBe(1)
+    expect(slides[0].relationships.length).toBe(1)
+  })
+
+  it('Compose layout tree', () => {
     const tree = <slide padding={30000}>
       <text height={100000}>a<i>a</i></text>
       <div flexGrow={1}><chart /></div>
       <text height={200000}>aa</text>
     </slide>
     const json = testRenderer.create(tree).toJSON()
-    const node = walkTree(json!)
+    const node = composeNodeTree(json!)
     node.setWidth(9144000)
     node.setHeight(6858000)
     node.calculateLayout()
-    logNode(node)
-    console.log(JSON.stringify(composeJSONTree(json!, node), null, 2))
+    // logNode(node)
+    const layoutJSON = composeLayoutedTree(json!, node)
+    expect(layoutJSON.children[0].type).toBe("text")
+    expect(layoutJSON.children[0].layout.height).toBe(100000)
+    expect(layoutJSON.children[1].layout.height).toBe(6858000 - 100000 - 200000 - 60000)
+    expect(layoutJSON.children[2].layout.height).toBe(200000)
+    expect(layoutJSON.children[2].layout.width).toBe(9144000 - 60000)
   })
 })
